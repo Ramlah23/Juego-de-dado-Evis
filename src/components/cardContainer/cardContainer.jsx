3710eabea325d03e69ab6f1e68f74ee1722ae765
Cinthia <cinthia_ramlah@hotmail.com>
2024-07-26 18:49:25 -0300

    se quitp etiqueta footer de card container

--- src/components/cardContainer/cardContainer.jsx
+++ src/components/cardContainer/cardContainer.jsx
@@ -21,54 +21,42 @@ export const CardContainer = ( { diceNumber } ) => {
         {diceNumber === 1 && <Card
         cardHeader="Hola soy Card 1!"
         cardImg={cardImg}
-        cardFooter={
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ipsam provident id laudantium, "
-        }
+        
        
         diceNumber={diceNumber}
       /> }
       {diceNumber === 2 && <Card
         cardHeader="Hola soy Card 2!"
         cardImg={cardImg2}
-        cardFooter={
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ipsam provident id laudantium, "
-        }
+        
         
         diceNumber={diceNumber}
       />}
       {diceNumber === 3 && <Card
         cardHeader="Hola soy Card 3!"
         cardImg={cardImg3}
-        cardFooter={
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ipsam provident id laudantium, "
-        }
+       
         
         diceNumber={diceNumber}
       />}
       {diceNumber === 4 && <Card
         cardHeader="Hola soy Card 4!"
         cardImg={cardImg4}
-        cardFooter={
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ipsam provident id laudantium, "
-        }
+        
         
         diceNumber={diceNumber}
       />}
       {diceNumber === 5 && <Card
         cardHeader="Hola soy Card 5!"
         cardImg={cardImg5}
-        cardFooter={
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ipsam provident id laudantium, "
-        }
+       
        
         diceNumber={diceNumber}
       />}
       {diceNumber === 6 && <Card
         cardHeader="Hola soy Card 6!"
         cardImg={cardImg6}
-        cardFooter={
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ipsam provident id laudantium, "
-        }
+        
         
         diceNumber={diceNumber}
       />}
