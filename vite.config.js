380502ec0e37dc3024b7323df6b84483ec07feb0
Cinthia <cinthia_ramlah@hotmail.com>
2024-07-26 18:30:19 -0300

    se modific el archivo vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build' // Asegúrate de que el directorio de salida sea 'build'
+  }
+});
